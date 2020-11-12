538ad6eb73b3d8d176ea0fd75508a594fb2b2cb8
luongdoan277 <luongdoan277@gmail.com>
2020-11-12 21:40:09 +0700

    New

---
+++ Models/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStores.Models
+{
+    public class SeedData
+    {
+        public static void DataDrink(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Drinks.Any())
+            {
+                context.Drinks.AddRange(
+                    new Drink
+                    {
+                        Name = "Caramel Brulée Latte",
+                        Description = "Our signature espresso, steamed milk and rich caramel brulée sauce finished with whipped cream and a supreme topping of even more caramel brulée.",
+                        Img = "Drink1.png",
+                        Price = 9,
+                        Discount = 10,
+                        Status = "new"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Models/StoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStores.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options) { }
+
+        public DbSet<Drink> Drinks { get; set; }
+    }
+}
